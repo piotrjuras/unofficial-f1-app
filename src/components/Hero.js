7e831d62f567edd9edde6bf555892841c7b576a2
Piotr Juras <91953085+piotrjuras@users.noreply.github.com>
2021-11-12 14:16:57 +0100

    Add files via upload

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,11 +2,14 @@ import React, { useEffect, useState } from 'react';
 import today from './TodaysDate';
 import './Hero.scss';
 
+import Button from './atoms/Button';
+
 import Countdown from './Countdown';
 
 const Hero = ({ races }) => {
 
     const [nextRace, setNextRace] = useState(false);
+    const [fullscreen, setFullscreen] = useState(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent));
 
     useEffect(() => {
 
@@ -21,6 +24,33 @@ const Hero = ({ races }) => {
     },[]) // eslint-disable-line
 
 
+    useEffect(() => {
+        window.addEventListener("fullscreenchange", fullScreenChange)
+
+        return () => {
+            window.removeEventListener("fullscreenchange", fullScreenChange)
+        }
+    })
+
+    const fullScreenChange = () => document.fullscreenElement ? setFullscreen(true) : setFullscreen(false);
+
+
+    const toggleFullScreen = () => {
+
+        if (!document.fullscreenElement && 
+            !document.mozFullScreenElement && !document.webkitFullscreenElement) {
+
+            if (document.documentElement.requestFullscreen) {
+                document.documentElement.requestFullscreen();
+                } else if (document.documentElement.mozRequestFullScreen) {
+                document.documentElement.mozRequestFullScreen();
+                } else if (document.documentElement.webkitRequestFullscreen) {
+                document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+                }
+            }
+
+      }
+
     return(
         nextRace ? (
             <header>
@@ -31,6 +61,8 @@ const Hero = ({ races }) => {
                     <h2><Countdown date={`${nextRace.date}T${nextRace.time.slice(0, -1)}`} /></h2>
                     <p>{nextRace.Circuit.circuitName}</p>
                     <p>{nextRace.date} at {nextRace.time.slice(0, -4)}</p>
+                    { fullscreen ? null : <Button onClick = {() => toggleFullScreen()}>Go to full-screen</Button> }
+
                 </main>
             </header>
         ) : null
